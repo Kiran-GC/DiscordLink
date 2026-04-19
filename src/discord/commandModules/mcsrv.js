939af35251c3dada1696b98654d7866b78f397ce
Kiran <84382139+Kiran-GC@users.noreply.github.com>
2026-04-19 07:01:10 +0530

    Add modular mcsrv command

---
+++ src/discord/commandModules/mcsrv.js
@@ -0,0 +1,45 @@
+const { AttachmentBuilder, SlashCommandBuilder } = require('discord.js');
+const { getStatus } = require('../../mc/status');
+const { buildSimpleEmbed } = require('../../embeds/simpleEmbed');
+const { commandCooldownReply } = require('../../utils/interactionReplies');
+
+const COOLDOWN_MS = 5000;
+const cooldowns = new Map();
+
+const data = new SlashCommandBuilder()
+    .setName('mcsrv')
+    .setDescription('Check any Minecraft server')
+    .addStringOption(option =>
+        option.setName('ip')
+            .setDescription('Server IP')
+            .setRequired(true)
+    );
+
+async function execute(client, interaction) {
+    const now = Date.now();
+    const cooldownUntil = cooldowns.get(interaction.user.id) || 0;
+
+    if (cooldownUntil > now) {
+        const seconds = Math.ceil((cooldownUntil - now) / 1000);
+        return interaction.reply(commandCooldownReply(seconds));
+    }
+
+    cooldowns.set(interaction.user.id, now + COOLDOWN_MS);
+
+    const ip = interaction.options.getString('ip');
+    const status = await getStatus(ip);
+
+    const embed = buildSimpleEmbed(status, ip);
+    const files = [];
+
+    if (status.icon && status.icon.startsWith('data:image')) {
+        const buffer = Buffer.from(status.icon.split(',')[1], 'base64');
+        const attachment = new AttachmentBuilder(buffer, { name: 'icon.png' });
+        files.push(attachment);
+        embed.setThumbnail('attachment://icon.png');
+    }
+
+    return interaction.reply({ embeds: [embed], files });
+}
+
+module.exports = { data, execute };
